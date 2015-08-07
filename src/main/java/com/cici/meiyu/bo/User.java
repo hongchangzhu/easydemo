4ca2d0d86e2801b5a7e3260c4b6023c2dcc5496a
hao_chen <hao_chen@kingdee.com>
2015-08-07 10:05:38 +0800

    配置

---
+++ src/main/java/com/cici/meiyu/bo/User.java
@@ -0,0 +1,13 @@
+package com.cici.meiyu.bo;
+
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}

274ea89f85da23216f9560bb69a5c50ed8ee09bf
hao_chen <hao_chen@kingdee.com>
2015-08-07 14:19:22 +0800

    例子

--- src/main/java/com/example/action/TestController.java
+++ src/main/java/com/example/action/TestController.java
@@ -1,14 +1,23 @@
 package com.example.action;
 
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller("testController")
-public class TestController {
+import com.framework.base.BaseController;
 
-	@RequestMapping(value = "/easyui", method = RequestMethod.POST, consumes = "application/json")
-	public String easyui() {
-		return null;
+@Controller
+@RequestMapping(value = "tc")
+public class TestController extends BaseController {
+	private Logger log = Logger.getLogger(TestController.class);
+
+	@RequestMapping(value = "stat")
+	public String stat() {
+		log.info("*********************");
+		// return this.SUCCESS(new HashMap<String, Object>());
+		return "test";
 	}
 }
